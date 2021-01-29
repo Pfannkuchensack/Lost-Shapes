3256d2d08f32f373b258725e603af6f5bbd1ddb5
Alexander Eichhorn <alex@code-with.us>
2021-01-29 19:51:48 +0100

    draw ball

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ require('./bootstrap');
 
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
