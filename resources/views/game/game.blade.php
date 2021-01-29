8aa3b87c363668363d702ac4262ad8b27144b1b1
Alexander Eichhorn <alex@code-with.us>
2021-01-29 22:59:15 +0100

    fix html

--- resources/views/game/game.blade.php
+++ resources/views/game/game.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Lost Shapes</title>
+		<title>Lost Shapes</title>
+		<meta name="gameid" content="{{ $gameid }}">
+		<meta name="color" content="{{ $color }}">
 
         <!-- Fonts -->
         <!--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">-->
