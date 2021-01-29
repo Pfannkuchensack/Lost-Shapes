42236ea47625cc90f33b766f96387ef045817d4b
Michael Matthé <michael.matthe@gmail.com>
2021-01-29 22:15:30 +0100

    Implement basic movement and collision detection

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,19 +2,177 @@ require('./bootstrap');
 
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
+var playerXPosition = 55;
+var playerYPosition = 150;
+var xPlayerSpeed = 2;
+var yPlayerSpeed = 2;
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+var shapeRadius = 10;
+
+var torch = false;
+var lighting = 50;
+
+// x1, y1, x2, y2
+var wallArray = [[50, 50, 60, 100], [60, 50, 100, 70], [120, 150, 130, 220]];
+
+const UP = 0;
+const DOWN = 1;
+const LEFT = 2;
+const RIGHT = 3;
+
+var rightPressed = false;
+var leftPressed = false;
+var upPressed = false;
+var downPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight" || e.key == "d") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft" || e.key == "a") {
+        leftPressed = true;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
+        upPressed = true;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown" || e.key == "s") {
+        downPressed = true;
+    }
+    else if(e.key == "x"){
+        torch = !torch;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+    else if(e.key == "Up" || e.key == "ArrowUp" || e.key == "w") {
+        upPressed = false;
+    }
+    else if(e.key == "Down" || e.key == "ArrowDown" || e.key == "s") {
+        downPressed = false;
+    }
+}
+
+function movePlayer() {
+
+    // right / left
+    if(rightPressed) {
+        playerXPosition += xPlayerSpeed;
+        wallCollision(RIGHT);
+    }    
+    if(leftPressed){
+        playerXPosition -= xPlayerSpeed;
+        wallCollision(LEFT);
+    }
+    // up / down
+    if(upPressed) {
+        playerYPosition -= yPlayerSpeed;
+        wallCollision(UP);
+    }
+    if(downPressed) {
+        playerYPosition += yPlayerSpeed;
+        wallCollision(DOWN);
+    }
+
+    // collision detection end of map
+    if(playerXPosition < 0 + shapeRadius){
+        playerXPosition = 0 + shapeRadius;
+    }
+    else if (playerXPosition > canvas.width - shapeRadius){
+        playerXPosition = canvas.width - shapeRadius;
+    }
+
+    if(playerYPosition < 0 + shapeRadius){
+        playerYPosition = 0 + shapeRadius;
+    }
+    else if (playerYPosition > canvas.height - shapeRadius){
+        playerYPosition = canvas.height - shapeRadius;
+    }
+
+}
+
+function wallCollision(pressed) {
+    wallArray.forEach(function(wall) {
+        //console.log(playerXPosition, playerYPosition, pressed);
+        if(pressed == UP) {
+            if(playerXPosition + shapeRadius >= wall[0] && playerXPosition - shapeRadius <= wall[2]){
+                if(playerYPosition - shapeRadius + yPlayerSpeed >= wall[3] && playerYPosition - shapeRadius <= wall[3]){
+                    playerYPosition = playerYPosition + yPlayerSpeed;
+                }
+            }
+        }    
+        if(pressed == DOWN){
+            if(playerXPosition + shapeRadius >= wall[0] && playerXPosition - shapeRadius <= wall[2]){
+                if(playerYPosition + shapeRadius - yPlayerSpeed <= wall[1] && playerYPosition + shapeRadius >= wall[1]){
+                    playerYPosition = playerYPosition - yPlayerSpeed;
+                }
+            }
+        }    
+        if(pressed == LEFT) {
+            if(playerYPosition + shapeRadius >= wall[1] && playerYPosition - shapeRadius <= wall[3]){
+                if(playerXPosition - shapeRadius + xPlayerSpeed >= wall[2] && playerXPosition - shapeRadius <= wall[2]){
+                    playerXPosition = playerXPosition + xPlayerSpeed;
+                }
+            }
+        }
+        if(pressed == RIGHT) {
+            if(playerYPosition + shapeRadius >= wall[1] && playerYPosition - shapeRadius <= wall[3]){
+                if(playerXPosition + shapeRadius - xPlayerSpeed <= wall[0] && playerXPosition + shapeRadius >= wall[0]){
+                    playerXPosition = playerXPosition - xPlayerSpeed;
+                }
+            }
+        }
+    });
+}
+
+function drawPlayer() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(playerXPosition, playerYPosition, shapeRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-    x += dx;
-    y += dy;
 }
+
+function drawPlayerFieldOfView(){
+    ctx.beginPath();
+    ctx.fillStyle = "#383838"
+    if(torch && lighting < 100){
+        lighting += 1;
+    }
+    else if(!torch && lighting > 50) {
+        lighting -= 1;
+    }
+    ctx.arc(playerXPosition, playerYPosition, lighting, 0, Math.PI*2);
+    ctx.rect(canvas.width, 0, -canvas.width, canvas.height);
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawWalls() {
+    wallArray.forEach(function(wall) {
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.rect(wall[0], wall[1], wall[2] - wall[0], wall[3] - wall[1]);
+        ctx.fill();
+        ctx.closePath();
+    });
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPlayer();
+    drawWalls();
+    drawPlayerFieldOfView();
+
+    movePlayer();    
+}
+
 setInterval(draw, 10);
