c1b5c2a5961d37eb6727b564dc1e4e1e1ca38020
Michael Matthé <michael.matthe@gmail.com>
2021-01-31 14:37:26 +0100

    Full vision

--- resources/js/app.js
+++ resources/js/app.js
@@ -383,7 +383,7 @@ function draw() {
 	drawPlayerNetwork();
     drawParts();
     drawPlayer();
-    drawPlayerFieldOfView();
+    //drawPlayerFieldOfView();
 	movePlayer();
 	if(playerParts == 4 && networkParts == 4)
 	{
